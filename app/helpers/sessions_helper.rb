557410d1677a7ecb5678236582d2804ba1550ac2
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-17 00:32:28 +0800

    Add sessions helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,27 @@
 module SessionsHelper
+	def sign_in(user)
+		session[:user_id] = user.id
+		self.current_user = user
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def sign_out
+		session[:user_id] = nil
+		self.current_user = nil
+	end
+
+	def current_user?(user)
+		user == current_user
+	end
 end
