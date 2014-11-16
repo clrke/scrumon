f692d6fa1cc10a58dd70c8d1579ec8ae4a6cfb9a
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-17 00:40:59 +0800

    Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
-
+  get 'sessions/new'
+  post 'sessions/create'
+  post 'sessions/destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
