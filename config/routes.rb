fcf25dfa0fb8cad7c36a569ccf3d52c6e53cf4a7
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-17 00:58:18 +0800

    Add login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'sessions/new'
-  post 'sessions/create'
+  post 'sessions/create', as: 'sessions'
   post 'sessions/destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
