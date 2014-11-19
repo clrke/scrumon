49fee510baf32ab6f333c157eb190796a114e50b
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-19 17:17:24 +0800

    Remove generated routes from generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'organizations/index'
-
-  get 'organizations/new'
-
-  get 'organizations/edit'
-
-  get 'organizations/show'
 
   get 'users/new'
   get 'users/show'
