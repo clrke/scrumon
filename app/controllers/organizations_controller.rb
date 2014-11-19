0fe347d01d0a22453791c54dac565e47fab18725
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-19 17:46:02 +0800

    Add check_auth for OrganizationsController

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,13 +1,24 @@
 class OrganizationsController < ApplicationController
   def index
+  	check_auth
   end
 
   def new
+  	check_auth
   end
 
   def edit
+  	check_auth
   end
 
   def show
+  	check_auth
+  end
+
+  def check_auth
+  	@user = current_user
+  	if @user.nil?
+  		redirect_to sessions_new_path
+  	end
   end
 end
