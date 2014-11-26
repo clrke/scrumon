3586316b22c4d08330a98ed82587e847ed9b1cfc
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-26 08:29:44 +0800

    Reindent user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
-  def create
-	@user = User.create params[:user].permit(:username, :email, :password, :password_confirmation)
-
-	if @user.save
-		redirect_to sessions_new_path
-	else
-		flash[:error] = "Invalid credentials"
-		render action: "new"
+	def new
+		@user = User.new
 	end
-  end
+	def create
+		@user = User.create params[:user].permit(:username, :email, :password, :password_confirmation)
 
-  def show
-  	@user = current_user
-  	if @user.nil?
-  		redirect_to sessions_new_path
-  	end
-  end
+		if @user.save
+			redirect_to sessions_new_path
+		else
+			flash[:error] = "Invalid credentials"
+			render action: "new"
+		end
+	end
+
+	def show
+		@user = current_user
+		if @user.nil?
+			redirect_to sessions_new_path
+		end
+	end
 end
