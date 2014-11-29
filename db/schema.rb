003452c26e2838427e599feedd8187a56c2be654
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-29 19:51:56 +0800

    Migrate tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117153607) do
+ActiveRecord::Schema.define(version: 20141129114354) do
 
   create_table "organizations", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20141117153607) do
     t.integer "user_id"
     t.boolean "admin"
   end
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.integer  "organization_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tasks", ["organization_id"], name: "index_tasks_on_organization_id"
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "username"
